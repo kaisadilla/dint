df35dc3383e8fa7344647d9734ee12408ceae90a
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2020-10-30 18:30:23 +0100

    Zombie Ipsum

---
+++ js/rudimentary-search-engine.js
@@ -0,0 +1,12 @@
+let results = [
+    {
+        name: "colores y paletas",
+        path: "les1/colors_and_palettes.html",
+        keywords: ["colores", "paletas", "paletton", "color wheel pro", "colorlovers", "materials"]
+    },
+    {
+        name: "google fonts",
+        path: "les1/google_fonts.html",
+        keywords: ["google", "fonts", "fuentes", "estilo"]
+    }
+]
